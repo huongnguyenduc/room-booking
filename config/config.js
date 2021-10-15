47134c1da30e9fc518a9121a9b85281673e8f1bf
Duc Huong <duchuong007@gmail.com>
2021-10-16 00:23:34 +0700

    Add color and font family

--- config/config.js
+++ config/config.js
@@ -35,7 +35,25 @@ export default defineConfig({
   routes,
   // Theme for antd: https://ant.design/docs/react/customize-theme-cn
   theme: {
-    'primary-color': defaultSettings.primaryColor,
+    'primary-color': '#FB6933',
+    'info-color': '#33A1FD',
+    'success-color': '#35B4AD',
+    'error-color': '#FF6B6B',
+    'warning-color': '#F2A63C',
+    'normal-color': '#d9d9d9',
+    white: '#fff',
+    black: '#000',
+    'font-family': 'Open Sans, serif',
+    'text-color': '@gray-1',
+    'gray-1': 'var(--gray-1)',
+    'gray-2': 'var(--gray-2)',
+    'gray-3': 'var(--gray-3)',
+    'gray-4': 'var(--gray-4)',
+    'gray-5': 'var(--gray-5)',
+    'gray-6': 'var(--gray-6)',
+    'gray-7': 'var(--gray-7)',
+    'gray-8': 'var(--gray-8)',
+    'gray-9': 'var(--gray-9)',
   },
   // esbuild is father build tools
   // https://umijs.org/plugins/plugin-esbuild
