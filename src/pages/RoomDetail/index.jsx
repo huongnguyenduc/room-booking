6fb78878d29d9be3bec1282043f550ec058676cb
Phạm Tấn khang <phamtankhang2001@gmail.com>
2021-11-11 22:04:00 +0700

    UI(complier): complete fix UI

--- src/pages/RoomDetail/index.jsx
+++ src/pages/RoomDetail/index.jsx
@@ -228,12 +228,6 @@ const RoomDetail = (props) => {
           <CardItem />
           <CardItem />
         </Carousel>
-        <Avatar
-          size={48}
-          src={
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80'
-          }
-        />
       </div>
     </Content>
   );
